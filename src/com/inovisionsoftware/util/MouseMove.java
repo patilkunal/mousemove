f641f588658780ad52a35fcf33e312e579a8023d
Kunal Patil <Kunal.Patil@cox.com>
2018-07-11 12:06:14 -0400

    Initial Commit

---
+++ src/com/inovisionsoftware/util/MouseMove.java
@@ -0,0 +1,68 @@
+package com.inovisionsoftware.util;
+
+public class MouseMove {
+
+	public static void main(String[] args) {
+
+		Monitor monitor = new Monitor();
+
+		boolean gui = true;
+		int hours = 0;
+		int minutes = 5;
+		int seconds = 0;
+
+		try {
+			
+			/* Parse arguments. The two possible arguments are:
+			 * -nogui
+			 * -interval hh:mm:ss
+			 * */
+			for (int i = 0; i < args.length; i++) {
+				if (args[i].equals("-interval")) {
+					i++;
+					String times[] = args[i].split(":");
+					hours = Integer.parseInt(times[0]);
+					minutes = Integer.parseInt(times[1]);
+					seconds = Integer.parseInt(times[2]);
+
+					if (minutes > 60 || seconds > 60) {
+						// TODO: Create specific exception
+						throw new Exception();
+					}
+					
+					if (hours == 0 && minutes == 0 && seconds == 0) {
+						// TODO: Create specific exception
+						throw new Exception("The delay must be greater than zero seconds");
+					}
+				} else if (args[i].equals("-nogui")) {
+					gui = false;
+				} else {
+					// TODO: Create specific exception
+					throw new Exception("No such argument");
+				}
+			}
+			
+			monitor.setTime(hours, minutes, seconds);
+			
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			System.err.println("Usage: java -jar wigglemouse [-nogui] [-interval hh:mm:ss]");
+			System.exit(1);
+		} 
+
+		/* We'll create a thread that will sleep for some seconds and
+		 * after those seconds it will wiggle the mouse 
+		 * */
+		WaitingThread waitingThread = new WaitingThread(monitor);
+		
+		if (gui) {
+			TextDialog textDialog = new TextDialog(monitor, waitingThread);	
+		} else {
+			System.out.println("Running with interval: "+hours+":"+minutes+":"+seconds);
+		}
+		
+
+		waitingThread.start();
+	}
+}
---
+++ src/com/inovisionsoftware/util/WaitingThread.java
@@ -0,0 +1,74 @@
+package com.inovisionsoftware.util;
+
+import java.awt.AWTException;
+import java.awt.MouseInfo;
+import java.awt.Point;
+import java.awt.Robot;
+
+
+public class WaitingThread extends Thread {
+	private Monitor monitor;
+	private Robot robot;
+
+	public WaitingThread(Monitor monitor) {
+		this.monitor = monitor;
+		try {
+			this.robot = new Robot();
+		} catch (AWTException e) {
+			System.err.println("Robot couldn't be created");
+			System.exit(1);
+		}
+	}
+
+	public void run() {
+		while (true) {
+			int secondsToSleep = (monitor.getHours()*60+monitor.getMinutes())*60+monitor.getSeconds();
+
+
+			try {
+				for (int i = 0; i < secondsToSleep; i++) {
+					Point point = MouseInfo.getPointerInfo().getLocation();
+					int oldX = (int)point.getX();
+					int oldY = (int)point.getY();
+
+					Thread.sleep(1000);
+
+					point = MouseInfo.getPointerInfo().getLocation();
+					int newX = (int)point.getX();
+					int newY = (int)point.getY();
+
+					if (newX != oldX || newY != oldY) {
+						throw new Exception();
+					}
+
+				}
+				wiggle();
+			} catch (InterruptedException e) {
+				/* Nothing wrong, the user has pressed the button 
+				 * which interrupts this thread
+				 */
+			} catch (Exception e) {
+				/* Nothing wrong, the user moved the mouse.
+				 * */
+			}
+		}
+	}
+
+	private void wiggle() {
+		Point point = MouseInfo.getPointerInfo().getLocation();
+		int x = (int)point.getX();
+		int y = (int)point.getY();
+
+
+		try {
+			robot.mouseMove(x, y+5);
+			Thread.sleep(50);
+			robot.mouseMove(x, y-5);
+			Thread.sleep(50);
+			robot.mouseMove(x, y);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
